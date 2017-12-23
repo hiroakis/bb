d7b3af0ef35045d42b16181e56c9f7e66f1a028f
Songmu <y.songmu@gmail.com>
2017-12-23 23:05:11 +0900

    add cli_test.go

---
+++ cli_test.go
@@ -0,0 +1,23 @@
+package bb
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	input := "16bf0f1e88de"
+	expect := "[]byte{0x16, 0xbf, 0x0f, 0x1e, 0x88, 0xde}"
+
+	buf := &bytes.Buffer{}
+	cl := &cli{outStream: buf, errStream: ioutil.Discard}
+	err := cl.run([]string{input})
+	if err != nil {
+		t.Errorf("err should be nil but: %s", err)
+	}
+	out := buf.String()
+	if out != expect {
+		t.Errorf("something went wrong. out: %s, expect: %s", out, expect)
+	}
+}
